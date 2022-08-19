968ef150f9cce92eb8199f7d5f67b663dced5636
josueSU-UNSA <82920949+josueSU-UNSA@users.noreply.github.com>
2022-08-18 23:57:49 -0500

    Actualización con mayor modularización

--- Lista_de_eventos/Lista de eventos_11.py
+++ Lista_de_eventos/Lista de eventos_11.py
@@ -1,3 +1,4 @@
+from re import S
 from typing import List
 
 #
@@ -22,20 +23,27 @@ class Lista_de_eventos:
 # Esta funcion nos permitira saber si existe un
 # evento ,buscando por su ids
 #
+def llenado_de_lista_prueba(lista_objeto,n):
+    for i in range(n):
+        lista_obj.agregar_evento(i)
+
 def buscar(lista_eventos_entrada_ids,id_evento_buscado):
     for i in lista_eventos_entrada_ids:
         if(id_evento_buscado==i):
             return True
     return False
+def entrada_id():
+    id_entero=input("Ingrese el id del evento: ")
+    return id_entero
 
+def mensaje(bolean_valor):
+    if(bolean_valor):
+        print("Encontrado")
+    else:
+        print("No encontrado")
 # Uso de la funcion
 
 lista_obj=Lista_de_eventos()
-
-for i in range(5):
-    lista_obj.agregar_evento(i)
-
-if(buscar(lista_obj.lista,4)):
-    print("Encontrado")
-else:
-    print("No encontrado")
+llenado_de_lista_prueba(lista_obj, 5)
+mensaje(buscar(lista_obj.lista,entrada_id()))
+lista_obj.mostrar_eventos()
